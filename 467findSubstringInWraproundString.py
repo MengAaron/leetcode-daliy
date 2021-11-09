13abf4a93e311da8c661ea74ade1357eaf32a982
MengAaron <871628893@qq.com>
2021-11-09 15:00:58 +0800

    daliy

--- 467findSubstringInWraproundString.py
+++ 467findSubstringInWraproundString.py
@@ -4,14 +4,27 @@
 # @Author: Aaron Meng
 # @File  : 467findSubstringInWraproundString.py
 from typing import List
+from collections import Counter
 
 
 class Solution:
     @staticmethod
     def findSubstringInWraproundString(p: str) -> int:
-        pass
+        letter_counter = Counter()
+        n = len(p)
+        letter_counter[p[0]] = 1
+        max = 1
+        for j in range(1, n):
+            diff = ord(p[j]) - ord(p[(j-1)])
+            if  diff == 1 or diff == -25:
+                max += 1
+            else:
+                max = 1
+            if max > letter_counter[p[j]]:
+                letter_counter[p[j]] = max
+        return sum(letter_counter.values())
 
 
 if __name__ == '__main__':
-    p = "zab"
+    p = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz"
     print(Solution.findSubstringInWraproundString(p))
